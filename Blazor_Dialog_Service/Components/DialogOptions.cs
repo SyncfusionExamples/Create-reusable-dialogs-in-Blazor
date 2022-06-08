2406e180e550e8d975d964b7c5d57ec54b11c269
Manikandan-Lakshmanan <93248283+Manikandan-Lakshmanan@users.noreply.github.com>
2022-06-08 12:36:14 +0530

    documentation(BLAZ-22961): sample Added

---
+++ Blazor_Dialog_Service/Components/DialogOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_Dialog_Service.Components
+{
+    public class DialogOptions
+    {
+        public string Header { get; set; }
+        public RenderFragment Content { get; set; }
+        public RenderFragment FooterTemplate { get; set; }
+        public bool IsModal { get; set; } = true;
+        public bool ShowCloseIcon { get; set; } = false;
+    }
+}
---
+++ Blazor_Dialog_Service/Components/DialogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_Dialog_Service.Components
+{
+    public class DialogService
+    {
+        public event Action<DialogOptions> DialogInstance;
+
+        public void Open(DialogOptions options)
+        {
+            // Invoke DialogComponent to update and show the dialog with options
+            this.DialogInstance.Invoke(options);
+        }
+    }
+}
