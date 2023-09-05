3962204bbe4b3bc2696c76b303f964a8ff4dc1d0
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-09-05 13:06:52 +0530

    Update project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+
+using CreateReusableDialog.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<SfDialogService>();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
